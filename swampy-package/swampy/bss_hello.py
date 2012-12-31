885fe3272600c34b62dd3123268943088941ac89
Margaret Scott <eudaimonious@gmail.com>
2012-12-31 12:12:39 -0500

    Partially Completed

--- swampy-package/swampy/bss_hello.py
+++ swampy-package/swampy/bss_hello.py
@@ -1,21 +1,100 @@
 # Hello excercise from Week 0
 
-# Name:
+# Name: Margaret Scott (eudaimonious)
 
 
 from TurtleWorld import * 		
 world = TurtleWorld()			
 bob = Turtle()				
 
-
-
 # This is where you put code to move bob
 
+#H starting position
+pu(bob)
+lt(bob)
+fd(bob, 100)
+lt(bob)
+fd(bob, 150)
+pd(bob)
+lt(bob)
 
+#H
+fd(bob, 100)
+lt(bob)
+lt(bob)
+fd(bob, 50)
+rt(bob)
+fd(bob, 25)
+rt(bob)
+fd(bob, 50)
+lt(bob)
+lt(bob)
+fd(bob, 100)
 
+#E starting position
+pu(bob)
+rt(bob)
+fd(bob, 15)
+pd(bob)
 
+#E
+fd(bob, 25)
+bk(bob, 25)
+rt(bob)
+fd(bob, 50)
+lt(bob)
+fd(bob, 25)
+bk(bob, 25)
+rt(bob)
+fd(bob, 50)
+lt(bob)
+fd(bob, 25)
 
+#L(1) starting position
+pu(bob)
+fd(bob, 15)
+lt(bob)
+fd(bob, 100)
+pd(bob)
 
+#L(1)
+bk(bob, 100)
+rt(bob)
+fd(bob, 25)
 
+#L(2) starting position
+pu(bob)
+fd(bob, 15)
+lt(bob)
+fd(bob, 100)
+pd(bob)
+
+#L(2)
+bk(bob, 100)
+rt(bob)
+fd(bob, 25)
+
+#O starting position
+pu(bob)
+fd(bob, 15)
+lt(bob)
+fd(bob, 100)
+pd(bob)
+
+#O
+bk (bob, 100)
+rt(bob)
+fd(bob, 25)
+lt(bob)
+fd(bob, 100)
+lt(bob)
+fd(bob, 25)
+
+#end position
+pu(bob)
+bk(bob, 35)
+lt(bob)
+fd(bob, 100)
+lt(bob)
 
 wait_for_user()					
--- swampy-package/swampy/bss_polygon.py
+++ swampy-package/swampy/bss_polygon.py
@@ -1,21 +1,43 @@
 # Polygon excercise from Week 0
 
-# Name:
+# Name: Margaret Scott (eudaimonious)
 
 
 from TurtleWorld import * 		
 world = TurtleWorld()			
 bob = Turtle()				
+bob.delay= 0.01
 
-
-
+'''
 # This is where you put code to move bob
+def square(t, length):
+	for i in range(4):
+		fd(t, length)
+		lt(t)
 
+square(bob, 15)
 
+def polygon(t, length, n):
+	for i in range(n):
+		fd(t, length)
+		lt(t, 360.0/n)
 
+polygon(bob, 30, 6)
 
+def circle(t, r):
+	polygon(t, (2*3.14*r)/r, r)
 
+circle(bob, 100)
+'''
+def arc(t, r, angle):
+	n = r
+	i = 0
+	while 360.0/n*i < angle:
+	#for i in range(int(n*angle/360)):
+		fd(t, 2*3.14*r/n)
+		lt(t, 360.0/n)
+		i += 1
 
-
+arc(bob, 100, 180)
 
 wait_for_user()					
