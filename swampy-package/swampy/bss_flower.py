324fdc807fa5d92070069a8800ded9c1d12b3f6f
eudaimonious <eudaimonious@gmail.com>
2013-01-02 18:11:42 -0500

    completed assignment

--- swampy-package/swampy/bss_flower.py
+++ swampy-package/swampy/bss_flower.py
@@ -1,22 +1,81 @@
 # Flower excercise (4.2) from Week 0
 
-# Name:
+# Name: Margaret Scott (eudaimonious)
 
+from TurtleWorld import *
+import math
+world = TurtleWorld()
+bob = Turtle()
+bob.delay = 0.01
 
-from TurtleWorld import * 		
-world = TurtleWorld()			
-bob = Turtle()				
+def polyline(t, n, length, angle):
+    """Draws n line segments.
 
+    t: Turtle object
+    n: number of line segments
+    length: length of each segment
+    angle: degrees between segments
+    """
+    for i in range(n):
+        fd(t, length)
+        lt(t, angle)
 
+def arc(t, r, angle):
+    """Draws an arc with the given radius and angle.
+
+    t: Turtle
+    r: radius
+    angle: angle subtended by the arc, in degrees
+    """
+    arc_length = 2 * math.pi * r * abs(angle) / 360
+    n = int(arc_length / 4) + 1
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+
+    # making a slight left turn before starting reduces
+    # the error caused by the linear approximation of the arc
+    lt(t, step_angle/2)
+    polyline(t, n, step_length, step_angle)
+    rt(t, step_angle/2)
+
+def petal(t, r, theta):
+  """Uses a turtle(t) to draw a flower petal
+  that of radius (r) and circle fraction (theta)."""
+  for i in range (2):
+    arc(t, r, theta)
+    lt(t, 180-theta)
+
+def flower(t, r, theta, num_petals):
+  """Draws num_petals given to fill 360 degree space."""
+  for i in range(num_petals):
+    petal(t, r, theta)
+    lt(t, 360.0/num_petals)
+
+def move(t, distance):
+  """Moves turtles without drawing."""
+  pu(t)
+  fd(t, distance)
+  pd(t)
 
 # This is where you put code to move bob
 
+move(bob, -100)
+flower(bob, 60, 60, 7)
+
+move(bob, 100)
+flower(bob, 40, 80, 10)
+
+move(bob, 100)
+flower(bob, 140, 20, 20)
+'''
+arc(bob, 80, 100)
+lt(bob)
+arc(bob, 80, 100)
+lt(bob)
+arc(bob, 80, 100)
+'''
 
 
 
-
-
-
-
-wait_for_user()					
+wait_for_user()
 
--- swampy-package/swampy/bss_polygon.py
+++ swampy-package/swampy/bss_polygon.py
@@ -3,12 +3,13 @@
 # Name: Margaret Scott (eudaimonious)
 
 
-from TurtleWorld import * 		
-world = TurtleWorld()			
-bob = Turtle()				
+from TurtleWorld import *
+world = TurtleWorld()
+bob = Turtle()
 bob.delay= 0.01
 
-'''
+import math
+
 # This is where you put code to move bob
 def square(t, length):
 	for i in range(4):
@@ -17,27 +18,45 @@ def square(t, length):
 
 square(bob, 15)
 
+def polylines(t, length, n, angle): #t is turtle, length is length of line, n is number of lines, angle is angle of turtle turn
+    for i in range(n):
+        fd(t, length)
+        lt(t, angle)
+
 def polygon(t, length, n):
-	for i in range(n):
-		fd(t, length)
-		lt(t, 360.0/n)
+    angle = 360.0/n
+    polylines(t, length, n, angle)
 
 polygon(bob, 30, 6)
 
-def circle(t, r):
-	polygon(t, (2*3.14*r)/r, r)
 
-circle(bob, 100)
-'''
-def arc(t, r, angle):
-	n = r
-	i = 0
-	while 360.0/n*i < angle:
-	#for i in range(int(n*angle/360)):
-		fd(t, 2*3.14*r/n)
-		lt(t, 360.0/n)
-		i += 1
+def circle(t, r):   #r is radius
+    circumference = 2*math.pi*r
+    num_sides = (circumference / 3) + 1
+    seg_length = circumference / num_sides
+    angle = 360.0 / num_sides
+    polylines(t, seg_length, int(num_sides), angle)
+
+circle(bob, 75)
+
+
+
+def arc(t, r, theta): #theta is degrees of arc
+    circumference = 2*math.pi*r
+    arc_length = theta / 360.0 * circumference
+    num_sides = theta / 3
+    seg_length = arc_length / num_sides
+    angle = theta / num_sides
+    polylines(t, seg_length, num_sides, angle)
 
 arc(bob, 100, 180)
 
-wait_for_user()					
+wait_for_user()
+
+# the following condition checks whether we are
+# running as a script, in which case run the test code,
+# or being imported, in which case don't.
+
+if __name__ == '__main__':
+    world = TurtleWorld()
+
